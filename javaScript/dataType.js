afc3dfe5259d76ad131d46d28d9e3ecef3ff84fe
surajkr120 <144226400+surajkr120@users.noreply.github.com>
2024-08-04 17:11:58 +0000

    dataType summary

--- javaScript/dataType.js
+++ javaScript/dataType.js
@@ -1,1 +1,34 @@
-console.log("Suraj kumar");
+// primitive dataType
+// 7 types => string,number,boolean,null,undefined,symbol,BigInt
+
+const score = 100
+const scorevalue = 100.2
+const isLoggedIn = false
+const outtemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(typeof anotherId);
+
+//non-primitive dataType(Refrence)
+
+// Array,objects,functions
+
+const heroes = ["shaktiman", "robert_downey", "thanos"]
+let myObj= {
+    name:"suraj kumar",
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof anotherIdid);
+
+// javaScript is a dynamic type language 
+
+
+
+
+
